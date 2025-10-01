840989949e27645378f674c3cbc9839d84f64968
Alex Lovos <alex@MacBook-Air-de-Alex.local>
2025-09-30 20:08:29 -0600

    Sin la conexion a la bd

---
+++ semana 9.sql
@@ -0,0 +1,16 @@
+--creamos la bd 
+CREATE DATABASE bd_comercial
+GO
+-- seleccionamos la bd 
+USE bd_comercial;
+GO
+--creamos la tabla que nos piden
+CREATE TABLE cliente(
+    id_cliente INT PRIMARY KEY IDENTITY(1,1),
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    direccion VARCHAR(150) NOT NULL,
+    telefono VARCHAR(20) NOT NULL
+)
+
+SELECT * FROM cliente;
